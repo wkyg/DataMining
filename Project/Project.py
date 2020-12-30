c3024f81c47054acc20bc6df9449f75a7f3603e1
proscawards <proscawards99@gmail.com>
2020-12-30 16:36:17 +0800

    new

--- Project/Project.py
+++ Project/Project.py
@@ -38,30 +38,63 @@ class LoanPredictor():
 		menubar.add_cascade(label = "Help", menu = helpMenu)	
 		
 		# Create Label
-		canvas = tk.Canvas(window, bg="pink",width="500",height = "30").grid(sticky = W, column=0,row=0) 
-		labelMain = tk.Label(window, bg="pink", fg="white", text ="Predictive Model", font=('Helvetica', 15, 'bold'), justify=LEFT).grid(sticky = W, column=0,row=0) 
+		canvas = tk.Canvas(window, bg="pink",width="500",height = "40").place(x=0,y=0)
+		labelMain = tk.Label(window, bg="pink", fg="white", text ="Predictive Model", font=('Helvetica', 15, 'bold')).place(x=5,y=5)
 		self.createSubCategory1()
 		self.createSubCategory2()
 		self.createSubCategory3()
-		self.createSubCategory4()	
+		self.createSubCategory4()
+		self.createSubCategory5()			
+		self.predictionButton()
 		
 	# Create Sub-Categories
 	# SubCategory1 = Employment_Type
 	def createSubCategory1(self):
-		labelSubCat1 = Label(window, text ="Employment Type", font=('Helvetica', 10), justify=LEFT).grid(sticky = W, column=0,row=2)
+		labelSubCat1 = Label(window, text ="Employment Type", font=('Helvetica', 10), justify=LEFT).place(x=5,y=50)
+		var = IntVar()
+		empType1 = Radiobutton(window, text="Employee", variable=var, value=1,command=self.createSubCategory1).place(x=5,y=70)
+		empType2 = Radiobutton(window, text="Employer", variable=var, value=2,command=self.createSubCategory1).place(x=5,y=90)
+		empType3 = Radiobutton(window, text="Fresh Graduate", variable=var, value=3, command=self.createSubCategory1).place(x=5,y=110)
+		empType4 = Radiobutton(window, text="Self Employment", variable=var, value=4, command=self.createSubCategory1).place(x=5,y=130)			
 		
 	# SubCategory2 = Credit_Card_types
 	def createSubCategory2(self):
-		labelSubCat1 = Label(window, text ="Type of Credit Cards", font=('Helvetica', 10), justify=LEFT).grid(sticky = W, column=0,row=3) 	
+		labelSubCat2 = Label(window, text ="Type of Credit Cards", font=('Helvetica', 10), justify=LEFT).place(x=5,y=170)
+		var = IntVar()
+		cardType1 = Radiobutton(window, text="Normal", variable=var, value=1,command=self.createSubCategory1).place(x=5,y=190)
+		cardType2 = Radiobutton(window, text="Gold", variable=var, value=2,command=self.createSubCategory1).place(x=5,y=210)
+		cardType3 = Radiobutton(window, text="Platinum", variable=var, value=3, command=self.createSubCategory1).place(x=5,y=230)			
+		
 
 	# SubCategory3 = Property_Type
 	def createSubCategory3(self):
-		labelSubCat1 = Label(window, text ="Type of Properties", font=('Helvetica', 10), justify=LEFT).grid(sticky = W, column=0,row=4) 
-
-	# SubCategory4 = Property_Type
+		labelSubCat3 = Label(window, text ="Type of Properties", font=('Helvetica', 10), justify=LEFT).place(x=5,y=270) 
+		var = IntVar()
+		propertyType1 = Radiobutton(window, text="Bungalow", variable=var, value=3, command=self.createSubCategory1).place(x=5,y=290)		
+		propertyType2 = Radiobutton(window, text="Condominium", variable=var, value=1,command=self.createSubCategory1).place(x=5,y=310)
+		propertyType3 = Radiobutton(window, text="Terrace", variable=var, value=2,command=self.createSubCategory1).place(x=5,y=330)
+		
+	# SubCategory4 = Loan_Amount
 	def createSubCategory4(self):
-		labelSubCat1 = Label(window, text ="Type of Properties", font=('Helvetica', 10), justify=LEFT).grid(sticky = W, column=0,row=5) 
+		labelSubCat4 = Label(window, text ="Loan Amount (RM)", font=('Helvetica', 10), justify=LEFT).place(x=300,y=50)
+		var = IntVar()
+		loanAmount1 = Radiobutton(window, text="100,000 - 300,000", variable=var, value=1,command=self.createSubCategory1).place(x=300,y=70)
+		loanAmount2 = Radiobutton(window, text="300,000 - 500,000", variable=var, value=2,command=self.createSubCategory1).place(x=300,y=90)
+		loanAmount3 = Radiobutton(window, text="500,000 - 700,000", variable=var, value=3, command=self.createSubCategory1).place(x=300,y=110)
+		loanAmount4 = Radiobutton(window, text="700,000 - 900,000", variable=var, value=4, command=self.createSubCategory1).place(x=300,y=130)			
 
+	# SubCategory5 = Monthly_Salary
+	def createSubCategory5(self):
+		labelSubCat5 = Label(window, text ="Monthly Salary (RM)", font=('Helvetica', 10), justify=LEFT).place(x=300,y=170)
+		var = IntVar()
+		cardType1 = Radiobutton(window, text="4,000 - 7,000", variable=var, value=1,command=self.createSubCategory1).place(x=300,y=190)
+		cardType2 = Radiobutton(window, text="7,000 - 10,000", variable=var, value=2,command=self.createSubCategory1).place(x=300,y=210)
+		cardType3 = Radiobutton(window, text="10,000 - 13,000", variable=var, value=3, command=self.createSubCategory1).place(x=300,y=230)	
+
+	def predictionButton(self):
+		predictBtn = Button(window, text ="Predict Now").place(x=220,y=450) 
+		
+		
 	# Create Machine Learning Techniques Functions
 	# Decision Tree Classifier On Selected in Menu
 	def runDT(self):
